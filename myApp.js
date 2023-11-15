1af84f968d4d16fa381d74cd41714f5d56b405b3
AxelSeanCP <104364854+AxelSeanCP@users.noreply.github.com>
2023-11-15 22:22:04 +0700

    learning express #1
    - Complete challenges #1
    - Complete challenges #2
    - Complete challenges #3

--- myApp.js
+++ myApp.js
@@ -1,10 +1,19 @@
 let express = require('express');
 let app = express();
 
+// #1
+// console.log("Hello World");
 
+// #2
+// app.get("/", (req,res) => {
+//     res.send("Hello Express");
+// });
 
-
-
+// #3
+// absolutePath = __dirname + "/views/index.html";
+// app.get("/", (req,res) => {
+//     res.sendFile(absolutePath);
+// })
 
 
 
